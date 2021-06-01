2f90cfec0943d24b4a3f14e83fd77aff2d882d73
whl <1023680139@qq.com>
2021-06-01 20:44:53 +0800

    fix: 注释

--- node/basicE/app.js
+++ node/basicE/app.js
@@ -19,13 +19,14 @@ app.use('/', express.static( path.join( __dirname, "/public/")));
 
 // 跨域请求允许
 app.all('*', (req, res, next)=>{
+    // 允许的域，*为任意域名
     res.header("Access-Control-Allow-Origin","*");
-    //允许的header类型
+    // 允许的header类型
     res.header("Access-Control-Allow-Headers","content-type");
-    //跨域允许的请求方式 
+    // 允许的请求方式 
     res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
     if (req.method.toLowerCase() === 'options')
-        res.send(200);  //让options尝试请求快速结束
+        res.send(200);  //让options尝试请求快速返回
     else
         next();
 });
