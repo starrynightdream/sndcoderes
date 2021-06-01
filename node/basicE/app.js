3a253a6df9b43123ec57f590b2cac1ae55fbea8a
whl <1023680139@qq.com>
2021-06-01 10:11:00 +0800

    feat: node的子线程执行笔记

--- node/basicE/app.js
+++ node/basicE/app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const cp = require('child_process');
 
 const express = require('express');
 const session = require('express-session');
@@ -22,6 +23,48 @@ app.post('/postUrl', async (req, res) =>{
     res.json({
         //res data
 
+    });
+});
+
+app.get('/run_script/', (req, res)=>{
+    // run script
+    let key = req.body.key;
+    let file = '';
+    switch(key)
+    {
+        case 'fileName1':
+            file = 'filePath1';
+            break;
+        default:
+            file = 'def or err'
+    }
+    if (file)
+        res.json({
+            stdout: 'no such file'
+        });
+    
+    // there is python script, changeable
+    const commend = `python ${ path.join(__dirname, file)}`;
+    let pyThread = cp.exec(commend, (err, stdout, stderr) =>{
+        if (err){
+            console.error(err);
+            res.json({
+                stdout: 'err'
+            });
+            return;
+        }
+        console.log('输出>');
+        console.log(stdout);
+        console.log('错误#');
+        console.log(stderr);
+
+        res.json({
+            stdout
+        });
+    });
+
+    pyThread.on('exit', (code) =>{
+        // 可做退出处理
     });
 });
 
