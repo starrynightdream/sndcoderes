cba0c57c94477c734d88eb1608e11df27108f067
whl <1023680139@qq.com>
2021-06-04 18:54:30 +0800

    feat: mysql时间戳格式化与mysql 的dao层模板

---
+++ node/mysqlDao.js
@@ -0,0 +1,36 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'easymall',
+    timezone: 'Hongkong',
+    charset: 'utf8',
+});
+
+connection.connect();
+
+// 基础写法
+// use let result = await dao.funcName
+const funcName = ({param1, param2}) =>{
+    // let sql = `select * from user where username = '${username}' and password = '${password}'`;
+    let sql = `sql line replace string '${param1}' number/double ${param2}`;
+    return new Promise( (resolve, reject) =>{
+
+        connection.query(sql, (err, result) =>{
+        
+            if (err)
+            {
+                console.log('err: ', err);
+                reject(err);
+                return;
+            }
+            resolve(result);
+            return result;
+        });
+    });
+}
+
+module.exports = {
+    funcName
+}
--- node/util.js
+++ node/util.js
@@ -17,6 +17,40 @@ const getAllIpAddress = () =>{
     return ans;
 }
 
+/**
+ * 
+ * @param {Date} date 希望格式化的时间，默认为当前
+ * @param {string} pattern 格式化模板， 默认 yyyy-MM-dd
+ * @returns 
+ */
+const mysqlDateFormate = function (date, pattern) {
+    if (!date) {
+        date = new Date;
+    } else {
+        if (!isDate(date)) {
+            date = new Date(date);
+        }
+    }
+    pattern = pattern || 'yyyy-MM-dd';
+    var y = date.getFullYear().toString();
+    var o = {
+        M: date.getMonth() + 1, //month
+        d: date.getDate(), //day
+        h: date.getHours(), //hour
+        m: date.getMinutes(), //minute
+        s: date.getSeconds() //second
+    };
+    pattern = pattern.replace(/(y+)/ig, function (a, b) {
+        return y.substr(4 - Math.min(4, b.length));
+    });
+    for (var i in o) {
+        pattern = pattern.replace(new RegExp('(' + i + '+)', 'g'), function (a, b) {
+            return (o[i] < 10 && b.length > 1) ? '0' + o[i] : o[i];
+        });
+    }
+    return pattern;
+}
+
 module.exports = {
-    getAllIpAddress
+    getAllIpAddress, mysqlDateFormate
 }
