a6f7c71257e5401ee21d4438c4363d6f610b067c
whl <1023680139@qq.com>
2021-06-24 20:25:37 +0800

    fix: 修正i声明缺失

--- js/script/downloadCanvas.js
+++ js/script/downloadCanvas.js
@@ -2,7 +2,7 @@
 // 下载页面所有canvas内的内容。
 let cs = document.getElementsByTagName('canvas');
 let a = document.createElement('a');
-
+let i = 0;
 for (let c of cs){
     a.href = c.toDataURL();
     a.download = `docu${i++}.png`;
